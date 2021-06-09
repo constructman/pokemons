7d1d1e56c1f5a1ce80c6030780ecf3f9f99edcd8
sadpunchman <famelow@gmail.com>
2021-06-09 20:31:40 +0300

    add router

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {  
+  BrowserRouter as SRouter,
+  Switch
+} from "react-router-dom";
+
+const Router = () => (
+  <SRouter>
+    <Switch>
+    </Switch>
+  </SRouter>
+);
+
+export default Router
