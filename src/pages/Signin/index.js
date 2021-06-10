935084259e30139fefe0dbbb079441846703dbe5
sadpunchman <famelow@gmail.com>
2021-06-10 13:26:21 +0300

    fix pr

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './index.css';
 
-
 const Signin = () => (
     <main className="main">
       <form className="form">
         <div className="logo"></div>
-        <input type="email" className="input first" placeholder="Email" />
-        <input type="password" className="input" placeholder="Password" />
-        <button type="submit" className="submit">
+        <input 
+        type="email" 
+        className="input first" 
+        placeholder="Email" 
+        />
+        <input 
+        type="password" 
+        className="input" 
+        placeholder="Password" 
+        />
+        <button 
+        type="submit" 
+        className="submit">
           Sign In
         </button>
       </form>
